fea613ffce7651911b4c842f732b4ef946f0549f
Baptiste Langlade <baptouuuu@gmail.com>
2020-02-01 18:44:36 +0100

    add bootstrap return type

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,19 +3,17 @@ declare(strict_types = 1);
 
 namespace Innmind\Router;
 
-use Innmind\Router\{
-    RequestMatcher\RequestMatcher,
-    UrlGenerator\UrlGenerator,
-    Loader\Yaml,
-};
 use Innmind\Url\Path;
 
+/**
+ * @return array{requestMatcher: RequestMatcher, urlGenerator: UrlGenerator}
+ */
 function bootstrap(Path ...$routes): array
 {
-    $routes = (new Yaml)(...$routes);
+    $routes = (new Loader\Yaml)(...$routes);
 
     return [
-        'requestMatcher' => new RequestMatcher($routes),
-        'urlGenerator' => new UrlGenerator($routes),
+        'requestMatcher' => new RequestMatcher\RequestMatcher($routes),
+        'urlGenerator' => new UrlGenerator\UrlGenerator($routes),
     ];
 }
