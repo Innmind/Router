389d3fc14f756971844355dd666f296af5ca6a0f
Baptiste Langlade <baptouuuu@gmail.com>
2022-12-18 17:47:10 +0100

    transform NoMatchingRouteFound into a LogicException

--- src/Exception/NoMatchingRouteFound.php
+++ src/Exception/NoMatchingRouteFound.php
@@ -3,6 +3,6 @@ declare(strict_types = 1);
 
 namespace Innmind\Router\Exception;
 
-final class NoMatchingRouteFound extends RuntimeException
+final class NoMatchingRouteFound extends LogicException
 {
 }
