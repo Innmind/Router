505e71aac88d857ef5b5db97a73432a88fe98276
Baptiste Langlade <baptouuuu@gmail.com>
2022-12-18 17:45:35 +0100

    declare the exception that can be thrown

--- src/UrlGenerator.php
+++ src/UrlGenerator.php
@@ -3,7 +3,10 @@ declare(strict_types = 1);
 
 namespace Innmind\Router;
 
-use Innmind\Router\Route\Name;
+use Innmind\Router\{
+    Route\Name,
+    Exception\NoMatchingRouteFound,
+};
 use Innmind\Url\Url;
 use Innmind\Immutable\Map;
 
@@ -11,6 +14,8 @@ interface UrlGenerator
 {
     /**
      * @param ?Map<non-empty-string, string|list<string>|list<array{string, string}>> $variables
+     *
+     * @throws NoMatchingRouteFound
      */
     public function __invoke(Name $route, Map $variables = null): Url;
 }
