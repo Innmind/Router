9f3973724f3a2b1cc24962a56664477fa31e3d1e
Baptiste Langlade <baptiste.langlade@hey.com>
2025-08-31 16:25:56 +0200

    discard errors

--- src/Component/Like.php
+++ src/Component/Like.php
@@ -23,6 +23,7 @@ trait Like
     #[\NoDiscard]
     public function map(callable $map): Component
     {
+        /** @psalm-suppress InvalidArgument */
         return $this
             ->toComponent()
             ->map($map);
@@ -38,6 +39,7 @@ trait Like
     #[\NoDiscard]
     public function flatMap(callable $map): Component
     {
+        /** @psalm-suppress InvalidArgument */
         return $this
             ->toComponent()
             ->flatMap($map);
@@ -53,6 +55,7 @@ trait Like
     #[\NoDiscard]
     public function pipe(Component|Provider $component): Component
     {
+        /** @psalm-suppress MixedArgumentTypeCoercion */
         return $this
             ->toComponent()
             ->pipe($component);
